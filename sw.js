fec24944ac3a5704b528147bbcd032e6281ce9fa
bhuvan591 <bhaskarsuri1@gmail.com>
2025-08-22 09:47:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker installed");
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('lockscreen-store').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
