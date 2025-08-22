9c33906160c866490106104c661dd5eb84370fb6
patialashahichaapshahitandoori-crypto <patialashahichaapshahitandoori@gmail.com>
2025-08-22 09:14:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
 self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
-  e.waitUntil(caches.open("lockscreen-cache").then(cache => {
-    return cache.addAll(["/", "/index.html"]);
-  }));
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+  console.log("Service Worker installed");
 });
