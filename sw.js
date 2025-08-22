2d634478d279425f32f40e642d2e58b82f47fb1c
patialashahichaapshahitandoori-crypto <patialashahichaapshahitandoori@gmail.com>
2025-08-22 08:58:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,12 @@
-// sw.js (registers and caches index + your original file)
-const CACHE_NAME = 'magic-lockscreen-v1';
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/magic_real_lockscreen_superbig_time_blacktext.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
-];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+self.addEventListener("install", e => {
+  console.log("Service Worker Installed");
+  e.waitUntil(caches.open("lockscreen-cache").then(cache => {
+    return cache.addAll(["/", "/index.html"]);
+  }));
 });
 
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then((resp) => resp || fetch(evt.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
